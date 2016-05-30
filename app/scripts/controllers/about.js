96e4a8112bb49719ab18d1f6613d62fc6c3de421
Adil Bouhouch <adilbouhouch@MacBook-Pro-van-Adil.local>
2016-05-30 20:30:01 +0200

    initial Yo groundwork

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tigerspikeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tigerspikeApp
+ */
+angular.module('tigerspikeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tigerspikeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tigerspikeApp
+ */
+angular.module('tigerspikeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
